4f2f82f7a1a06e7b63697862e5eceb42f2bbabaf
unknown <wieserh@hertz-a.inet.dkfz-heidelberg.de>
2015-07-07 13:34:04 +0200

    added missing file

--- BioDataGeneration/getSigmaSISsq.m
+++ BioDataGeneration/getSigmaSISsq.m
@@ -1,5 +1,21 @@
 function [Sigma_SISsq] = getSigmaSISsq(Identifier,basePath)
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% getSigmaSISsq script
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright 2015, Hans-Peter Wieser
+%
+% h.wieser@dkfz.de
+%
+% This file is NOT part of the official matRad release. 
+% This file has to be used only for internal purposes! 
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% This script can be used to parse the first foci of a sis file. Please
+% note that it returns the sigma squared
+
 switch Identifier
     case 'C'
         FullFilePath = [basePath filesep 'SIS' filesep '12C_1.6.2008.sis'];
@@ -27,7 +43,8 @@ while(counter < 261)
    
         if(strfind(currentline,'energy'))
         energy(i)  = str2double(currentline(8:13));    
-        focus = cell2mat(textscan(currentline,'energy %*f focus %f %f %f %f %f %f %f')); %at the HIT facility only beams with a FWHM >=6mm are used
+        %at the HIT facility only beams with a FWHM >=6mm are used
+        focus = cell2mat(textscan(currentline,'energy %*f focus %f %f %f %f %f %f %f')); 
         ix = find(focus>=6,1);
         FWHM(i) = focus(ix);
         i = i+1;
